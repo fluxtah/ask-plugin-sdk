be04b2474086957732e51f1252f484c7cbb9cb12
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-07 16:38:46 +0100

    first

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import java.util.*
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("maven-publish")
+}
+
+val versionProps = Properties().apply {
+    load(file("version.properties").inputStream())
+}
+val appVersion = versionProps["version"].toString()
+
+group = "com.github.fluxtah"
+version = appVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "ask-plugin-sdk"
+
